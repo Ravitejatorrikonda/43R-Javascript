7308cbf40cef615a9dafd2cca01fab9fb114a4b4
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-08-05 12:50:08 +0530

    fun-1

---
+++ Day-7/index.js
@@ -0,0 +1,102 @@
+/**
+ * Functions in Javascript:-
+ * -----------------------
+ * functions are reusable block of code, function are make our code organize,
+ * Named functions are hoisted
+ * 
+ * they are diff types:-
+ * 1.Named frunction
+ * 2.First Class function
+ * 3.Anonymous function
+ * 4.Arrow function
+ * 5.callback function
+ * 6.constructer function
+ * 7.pure functions
+ * 8.Impuer functions
+ * 9.higer order function
+ * 10.lower order function
+ * 11.default parameter function
+ * 12.optional parameter function
+ * 13.rest parameter function
+ * 14.Immediate invoked function
+ * 15.Generatore function
+ * 
+ * 
+ * sy:-
+ * function defination:-
+ * ----------------------
+ *     function funName([parameters]){
+ *        //statements
+ *         [ return value]
+ * }
+ * 
+ * function defination is always stored in funName
+ * 
+ * function calling:-
+ * --------------------
+ * 
+ * funName([arguments])
+ */
+
+// function data(){
+
+//     console.log("data Printed")
+
+// }
+// console.log(data)
+// data()
+
+// function FunOne(){
+//     console.log("cocking started")
+//     order()
+//     console.log("coocking ended")
+// }
+// function order(){
+//     console.log("coocking stop");
+//     console.log("Order Rece");
+//     console.log("back to kitchen");
+    
+    
+// }
+// FunOne()
+
+// function funOne(){
+//     console.log("Enter")
+//     console.log("Exist")
+// }
+// funOne()
+// funOne()
+// funOne()
+
+// function one(){
+//     console.log("one")
+//     three()
+// }
+// function two(){
+//     console.log("two")
+// }
+// function three() {
+//     console.log("Three");
+//     two()
+    
+// }
+// function four() {
+//     console.log("Four");
+//     one()
+//     console.log("three");
+//     two()
+    
+    
+// }
+// four()
+
+// function sum(a,b){
+//     console.log(a-b)
+// }
+// sum("javascript")//error
+function a(){
+  console.log("a")
+}
+console.log(a);
+
+
