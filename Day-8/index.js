75004f41e2f1ee8776903827f6ab0ccc62fea345
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-08-06 12:27:07 +0530

    Fun-2

---
+++ Day-8/index.js
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+// function funOne(a,b){
+//     console.log(a+""+b)
+//     funTwo(a+b)
+// }
+// function funTwo(a){
+//     console.log(a)
+// }
+// funOne(12,14)
+
+// function one(a,b,c){
+//     console.log(a+b,b+c)
+//     console.log("first Function")
+//     two(a,b+c)
+// }
+// function two(a,b){
+//     console.log(a+b);
+//     console.log("Second function")
+// }
+// one(10,20,30)
+
+// function devara(hero,maincharachter){
+//     console.log(hero+maincharachter)
+//     pushpa(hero,maincharachter)
+//     console.log("devara")
+//     pushpa("allu","pavan")
+// }
+// function pushpa(hero,villan){
+//     console.log(hero+""+villan)
+//     console.log("pushpa")
+// }
+// devara("NTR","prakash")
+
+/**
+ * function funname([parameters]){
+ * //statements
+ * [return value]
+ * }
+ * funname()
+ * 
+ */
+
+// function funOne(){
+//     return "javascript"
+//     console.log("html")
+// }
+// console.log(funOne())
+function add(a){
+    return a+10
+}
+// console.log(add(5));
+// var result=add(5)
+// console.log(result)
+// var a=10;
+// function add(){
+//     var a=10;
+//     console.log(a)
+// }
+// add()
+// console.log(a)
+
+// function x(){
+//     console.log(i)
+//     return "hello"
+//      var i;
+// }
+// console.log(x())
