345c232af772984de475529a68586f584b2e9fc3
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-10-22 14:57:39 +0530

    clousers

---
+++ Day-30/index.js
@@ -0,0 +1,74 @@
+/**
+ * v8 engine
+ * data types,variables
+ * GEC
+ * FEC
+ * Operatores
+ * conditional statements
+ * control statements
+ * functions
+ *    named fun,callback,anonymous,higher and lower ,first class,arrow functions
+ * callback hell
+ * promises
+ * fetch ->json
+ * array methods
+ * synchronous 
+ * asynch and await
+ * error handling
+ * scopes
+ * DOM
+ * Events
+ * User and admin
+ * 
+ * clousers
+ * object method
+ * BOM
+ * 
+ * clousers:-
+ * -----------
+ * 
+ * 
+ * 
+ */
+
+// function a() {
+//     let x = 34;
+//     function b(){
+//         console.log(x);
+
+//     }
+//     return b;
+// }
+// let result= a()
+// result()
+
+// const createCounter = (intialvalue) => {
+//     let counter = intialvalue;
+//     return (updatedcounter) => {
+//         counter += updatedcounter
+//         console.log(counter)
+//     }
+// }
+//  let addcounter= createCounter(3)
+//     addcounter(1)
+
+
+// for(var i=1; i<=5;i++){
+//     // setTimeout(()=>{
+//     //     console.log(i)
+//     // },3000)
+// }
+// console.log(i)
+
+
+// let reg=new RegExp("abc")
+// console.log(reg)
+
+// let reg=/^[?=.*(a-b)].{3,16}$/
+// console.log(reg.test("abc"))
+
+//BOM:-(Browser object model)
+
+localStorage.setItem("product", "venky")
+let result=localStorage.getItem("product")
+console.log(result)
